6f5ba94dce682bff0248ae01ac477a82a7c89dd2
Ivan Rendon <ivansito87@gmail.com>
2019-08-17 18:19:47 -0400

    adds button style and jumbotron image to main page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,7 +9,27 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+      <div className="container">
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <img src="http://origin-www.timewarner.com/sites/default/files/ckeditor/public/files/att_wm_hz_pref_rgb_pos.png" width="400" height="30"
+                 className="d-inline-block align-top" alt="" />
+
+            <span className="font-weight-bold">{""}</span>
+
+          </a>
+          {/*<a className="navbar-brand" href="#">
+            <img src={require("../../images/imageNotFound.jpg")} width="30" height="30" alt="" />
+          </a>*/}
+          <Link
+            onClick={this.props.handleSaved}
+            className="btn btn-outline-warning pull-right"
+            to=""
+          >
+            My Movies
+          </Link>
+        </nav>
+      {/*<nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
         <h4 className="navbar-brand" to="/">
           Warner Media Movie Search
         </h4>
@@ -26,7 +46,8 @@ class Nav extends Component {
             </li>
           </ul>
         </div>
-      </nav>
+      </nav>*/}
+      </div>
     );
   }
 }
